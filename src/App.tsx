bf12a5af172c690c4abbbc4d7ce40cd89bb23395
Juliet Cordor <grassojames5@gmail.com>
2022-03-03 19:06:09 +1100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import { Terminal } from 'xterm';
+import 'xterm/css/xterm.css';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const terminalRef = useRef<HTMLDivElement>(null);
+  const [terminal, setTerminal] = useState(new Terminal());
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  useEffect(() => {
+    setTerminal(new Terminal());
+    if (terminalRef.current) {
+      terminal.open(terminalRef.current);
+    }
+
+    return () => {
+      terminal.dispose();
+    };
+  }, [terminalRef]);
+
+  return <div id="terminal" ref={terminalRef}></div>;
 }
 
-export default App
+export default App;
